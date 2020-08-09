401f64cb14a26bc13a954979e85d908d7b23506b
wbh1328551759 <1328551759@qq.com>
2020-08-09 21:56:03 +0800

    完成上传结果部分内容

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,21 +1,40 @@
 import React,{ useRef } from 'react'
 import { useStores } from '../stores'
 import { observer } from 'mobx-react'
-import { Upload } from 'antd';
+import { Upload, message } from 'antd';
 import { InboxOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
 const { Dragger } = Upload;
 
 
 
+const Result = styled.div`
+  margin-top: 30px;
+  border: 1px dashed #ccc;
+  padding: 20px;
+`
+const H1 = styled.h1`
+  margin: 20px 0;
+  text-align: center;
+`
+const Image = styled.img`
+  max-width: 300px;
+`
+
+
 
 const Component = observer(() => {
-  const { ImageStore } = useStores()
+  const { ImageStore,UserStore } = useStores()
   const props = {
     showUploadList: false,
     beforeUpload: file => {
       console.log(file)
       ImageStore.setFile(file)
       ImageStore.setFilename(file.name)
+      if(!UserStore.currentUser){
+        message.warning('请先登录再上传！',3)
+        return false
+      }
       ImageStore.upload()
         .then(serverFile => {
           console.log('上传成功')
@@ -26,9 +45,6 @@ const Component = observer(() => {
       return false;
     },
   }
-
-
-
 
   return (
     <div>
@@ -42,12 +58,27 @@ const Component = observer(() => {
           band files
         </p>
       </Dragger>
-      <div>
-        <h1>上传结果</h1>
-        {
-          ImageStore.serverFile? <div>{ImageStore.serverFile.attributes.url.attributes.url}</div>: null
-        }
-      </div>
+      {
+        ImageStore.serverFile ? <Result>
+          <H1>上传结果</H1>
+          <dl>
+            <dt>线上地址：</dt>
+            <dd>
+              <a target="_blank" rel="noopener noreferrer" href={ImageStore.serverFile.attributes.url.attributes.url}>
+                { ImageStore.serverFile.attributes.url.attributes.url }
+              </a>
+            </dd>
+            <dt>文件名：</dt>
+            <dd>{ ImageStore.filename }</dd>
+            <dt>图片预览：</dt>
+            <dd>
+              <Image src={ImageStore.serverFile.attributes.url.attributes.url} alt=""/>
+            </dd>
+            <dt>更多尺寸</dt>
+            <dd>...</dd>
+          </dl>
+        </Result>:null
+      }
     </div>
   )
 })
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,22 +2,16 @@ import React from 'react'
 import { observer } from 'mobx-react'
 import {useStores} from '../stores'
 import Uploader from '../components/Uploader'
+import Tips from '../components/Tips'
 
 const Home = observer(() => {
   const { UserStore } = useStores()
 
   return(
     <>
-      <h1>
-        { UserStore.currentUser?
-          <>
-            Hello
-            <p>
-              {UserStore.currentUser.attributes.username}
-            </p>
-          </>:
-          '用户没有登陆'
-        }</h1>
+      <Tips>
+        请先登录再上传！
+      </Tips>
       <Uploader/>
     </>
   )
